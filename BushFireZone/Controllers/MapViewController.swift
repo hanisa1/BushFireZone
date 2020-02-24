7f4fe22b061c805f826172b7c805047f5630f43a
Hanisa Mohamed <hanisamohamed@Hanisas-MacBook-Pro.local>
2020-02-25 00:29:55 +1100

    Added custom annotation images

--- BushFireZone/Controllers/MapViewController.swift
+++ BushFireZone/Controllers/MapViewController.swift
@@ -37,18 +37,63 @@ class MapViewController: UIViewController, MKMapViewDelegate{
         // Do any additional setup after loading the view.
         
         setUpLayout()
-        setupCard()
         addAnnotations()
+        setupCard()
     }
 
     // Set up annotations for each Bush Fire Zone
     
-    let northCoastAnnotation = MKPointAnnotation()
-    let midNorthCoastAnnotation = MKPointAnnotation()
-    let hunterAnnotation = MKPointAnnotation()
     
     fileprivate func addAnnotations() {
+        
+        let mapView = MKMapView()
+                mapView.delegate = self
+                view.addSubview(mapView)
+                mapView.region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: -33.872760, longitude: 151.205340), span: .init(latitudeDelta: 10, longitudeDelta: 10 ))
+                mapView.fillSuperview()
+                
+                
+        
         let sydneyAnnotation = MKPointAnnotation()
+        sydneyAnnotation.title = "Sydney"
+        sydneyAnnotation.coordinate = CLLocationCoordinate2D(latitude: -33.872760, longitude: 151.205340)
+        
+        let northCoastAnnotation = MKPointAnnotation()
+        northCoastAnnotation.title = "North Coast"
+        northCoastAnnotation.coordinate = CLLocationCoordinate2D(latitude: -28.791719, longitude: 153.062866)
+        
+        let midNorthCoastAnnotation = MKPointAnnotation()
+        midNorthCoastAnnotation.title = "Mid North Coast"
+        midNorthCoastAnnotation.coordinate = CLLocationCoordinate2D(latitude: -29.064530, longitude: 153.349430)
+        
+        let hunterAnnotation = MKPointAnnotation()
+        hunterAnnotation.title = "Hunter"
+        hunterAnnotation.coordinate = CLLocationCoordinate2D(latitude: -34.071730, longitude: 150.637460)
+        
+        let blueMountainsAnnotation = MKPointAnnotation()
+        hunterAnnotation.title = "Blue Mountains"
+        hunterAnnotation.coordinate = CLLocationCoordinate2D(latitude: -33.714364, longitude: 150.311535)
+        
+        let southernHighlandsAnnotation = MKPointAnnotation()
+        hunterAnnotation.title = "Southern Highlands"
+        hunterAnnotation.coordinate = CLLocationCoordinate2D(latitude: -35.162214, longitude: 138.937631)
+        
+        let southCoastAnnotation = MKPointAnnotation()
+        hunterAnnotation.title = "South Coast"
+        hunterAnnotation.coordinate = CLLocationCoordinate2D(latitude: -34.892588, longitude: 150.582679)
+        
+        let riverinaAnnotation = MKPointAnnotation()
+        hunterAnnotation.title = "Riverina"
+        hunterAnnotation.coordinate = CLLocationCoordinate2D(latitude: -34.640830, longitude: 145.549860)
+        
+        let snowyMountainsAnnotation = MKPointAnnotation()
+        hunterAnnotation.title = "Snowy Mountains"
+        hunterAnnotation.coordinate = CLLocationCoordinate2D(latitude: -36.163541, longitude: 148.683154)
+        
+        let annotationList = [sydneyAnnotation, northCoastAnnotation, midNorthCoastAnnotation, hunterAnnotation, blueMountainsAnnotation, southernHighlandsAnnotation, southCoastAnnotation, riverinaAnnotation, snowyMountainsAnnotation]
+        annotationList.forEach { (ann) in
+            mapView.addAnnotation(ann)
+        }
     }
     
     fileprivate func animateTransitionIfNeeded(state: CardState, duration: TimeInterval) {
@@ -79,6 +124,7 @@ class MapViewController: UIViewController, MKMapViewDelegate{
         cardViewController = CardViewController()
         self.addChild(cardViewController)
         view.addSubview(cardViewController.view)
+        view.bringSubviewToFront(cardViewController.view)
         
         cardViewController.view.frame = CGRect(x: 0, y: self.view.frame.height - cardHandleAreaHeight, width: self.view.bounds.width, height: cardHeight)
         cardViewController.view.clipsToBounds = true
@@ -91,17 +137,18 @@ class MapViewController: UIViewController, MKMapViewDelegate{
     }
     
     fileprivate func setUpLayout() {
-        let mapView = MKMapView()
-        mapView.delegate = self
-        view.addSubview(mapView)
-        mapView.region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: -33.88, longitude: 151.20), span: .init(latitudeDelta: 10, longitudeDelta: 10))
-        mapView.fillSuperview()
         
-//        sydneyAnnotation.coordinate = CLLocationCoordinate2D(latitude: -33.88, longitude: 151.20)\
     }
     
     // gesture recognisers for the cardViewController
     @objc fileprivate func handleTap(recogniser: UITapGestureRecognizer) {
+        
+        switch recogniser.state {
+        case .ended:
+            animateTransitionIfNeeded(state: nextState, duration: 0.9)
+        default:
+            break
+        }
         
     }
     
@@ -148,5 +195,23 @@ class MapViewController: UIViewController, MKMapViewDelegate{
         }
     }
     
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: "AnnotationView")
+        
+        if annotationView == nil {
+            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: "AnnotationView")
+        }
+        
+        annotationView?.image = #imageLiteral(resourceName: "flame")
+        annotationView?.canShowCallout = true
+        
+        return annotationView
+    }
+    
+    
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        print("The annotation was selected: \(view)")
+    }
 }
 
